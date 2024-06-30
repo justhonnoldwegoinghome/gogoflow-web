64c8ebf04d58cb40275fa280e8fe20148bafe287
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-06-30 14:27:30 +0800

    add shadcn dialog

---
+++ src/components/dialog/DialogHeader.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/utils/cn";
+
+export function DialogHeader({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col space-y-1.5 text-center sm:text-left",
+        className
+      )}
+      {...props}
+    />
+  );
+}
