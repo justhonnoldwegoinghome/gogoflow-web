55dd3d0692bd7ba6d2e67892cc56db4b60b35b43
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-07-02 18:31:09 +0800

    style

--- src/features/users/components/UserDropdownMenu.tsx
+++ src/features/users/components/UserDropdownMenu.tsx
@@ -32,7 +32,7 @@ export function UserDropdownMenu({ id }: UserMenuProps) {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger className="w-12 h-12 rounded-full">
         <img
           className="w-12 h-12 rounded-full"
           src="https://images.unsplash.com/photo-1608889175250-c3b0c1667d3a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
