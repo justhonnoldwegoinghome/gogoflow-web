0cf737b948fb3c1c580b5ce377ba04cd47ca285d
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-07-04 19:31:43 +0800

    use swr infinite for conversation list

--- src/features/chat/api/getConversationList.ts
+++ src/features/chat/api/getConversationList.ts
@@ -1,4 +1,6 @@
-import useSWR from "swr";
+import _ from "lodash";
+import { useMemo } from "react";
+import useSWRInfinite from "swr/infinite";
 
 import { APIList, get } from "@/apiClient";
 import { Company } from "@/features/companies";
@@ -16,7 +18,7 @@ export function getConversationList({
   source: "shopee";
   pageSize: number;
   convoType: "all" | "pinned" | "unread";
-  nextPageToken?: string;
+  nextPageToken: string | null;
 }) {
   return get<APIList<Conversation>>("/conversations", {
     params: {
@@ -29,26 +31,98 @@ export function getConversationList({
   });
 }
 
-export function useConversationList({
+export function useConversationListInfinite({
+  pageSize,
   companyId,
   source,
-  pageSize,
   convoType,
-  nextPageToken,
 }: {
+  pageSize: number;
   companyId: Company["id"];
   source: "shopee";
-  pageSize: number;
-  convoType: "all" | "pinned" | "unread";
-  nextPageToken?: string;
+  convoType: "all" | "unread" | "pinned";
 }) {
-  return useSWR(`/companies/${companyId}/conversations`, () =>
-    getConversationList({
-      companyId,
-      source,
-      pageSize,
-      convoType,
-      nextPageToken,
-    })
+  const { data, size, setSize } = useSWRInfinite(
+    (idx, previousPageData) =>
+      getKey(idx, pageSize, companyId, previousPageData, source, convoType),
+    (k) =>
+      getConversationList({
+        companyId,
+        source,
+        pageSize,
+        convoType,
+        nextPageToken: k.pageToken,
+      }),
+    {
+      revalidateFirstPage: false, // issue 1401 but not needed for less dynamic apps like this
+    }
   );
+
+  const hasEnded = useMemo(() => {
+    if (data && data[data.length - 1].next_page_token === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }, [data]);
+
+  return {
+    data: data
+      ? {
+          results: _.flatten(data.map((d) => d.results)),
+        }
+      : undefined,
+    hasEnded,
+    loadMore: () => setSize(size + 1),
+  };
+}
+
+function getKey(
+  pageIndex: number,
+  pageSize: number,
+  companyId: Company["id"],
+  previousPageData: APIList<Conversation>,
+  source: "shopee",
+  convoType: "all" | "unread" | "pinned"
+) {
+  // In the documentation, SWR uses `key` for two purposes:
+  // 1) Serve as an index for its cache
+  // 2) Server as a url for its fetcher
+
+  // We typically use it only for 1).
+  // But in this case, while we do not use it directly as a url, we still use it for purpose 2) in that we use it to store our nextPageToken.
+
+  // First page
+  if (pageIndex === 0)
+    return {
+      pageIndex,
+      pageSize,
+      companyId,
+      resource: "conversations",
+      source,
+      convoType,
+      pageToken: null,
+    };
+
+  // Last page
+  if (previousPageData && !previousPageData.next_page_token)
+    return {
+      pageIndex,
+      pageSize,
+      companyId,
+      resource: "conversations",
+      source,
+      convoType,
+      pageToken: null,
+    };
+
+  return {
+    pageIndex,
+    pageSize,
+    companyId,
+    resource: "conversations",
+    source,
+    convoType,
+    pageToken: previousPageData.next_page_token,
+  };
 }
--- src/features/chat/components/CompanyConversationList.tsx
+++ src/features/chat/components/CompanyConversationList.tsx
@@ -1,32 +1,43 @@
 import { Spinner } from "@/components/spinner";
 import { Company } from "@/features/companies";
 
-import { useConversationList } from "../api/getConversationList";
+// import { useConversationList } from "../api/getConversationList";
+import { useConversationListInfinite } from "../api/getConversationList";
 import { ConversationCardUI } from "./ConversationCardUI";
 import { TypographyH1 } from "@/components/typography";
+import { Button } from "@/components/button";
 
 interface CompanyConversationListProps {
   id: Company["id"];
 }
 
 export function CompanyConversationList({ id }: CompanyConversationListProps) {
-  const conversationListQuery = useConversationList({
+  const { data, hasEnded, loadMore } = useConversationListInfinite({
     companyId: id,
     source: "shopee",
     pageSize: 20,
     convoType: "all",
   });
-  if (!conversationListQuery.data) return <Spinner />;
+
+  if (!data) return <Spinner />;
 
   return (
     <div className="max-w-screen-tablet mx-auto">
       <TypographyH1>Conversations</TypographyH1>
       <br />
       <div className="flex flex-col gap-8">
-        {conversationListQuery.data.results.map((c, i) => (
+        {data.results.map((c, i) => (
           <ConversationCardUI key={i} conversation={c} />
         ))}
       </div>
+      <br />
+      {hasEnded ? (
+        <Button disabled variant="outline">
+          End of list
+        </Button>
+      ) : (
+        <Button onClick={loadMore}>Load more</Button>
+      )}
     </div>
   );
 }
