3ec3c5dc0ddadd87f626f5452b7a240a7e5360ea
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-07-04 14:50:54 +0800

    add shadcn avatar

--- src/features/users/components/UserDropdownMenu.tsx
+++ src/features/users/components/UserDropdownMenu.tsx
@@ -1,6 +1,7 @@
 import { Building2, LogOut } from "lucide-react";
 import { useRouter } from "next/router";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -26,17 +27,20 @@ export function UserDropdownMenu({ id }: UserMenuProps) {
 
   const userQuery = useUser({ id });
   if (!userQuery.data)
-    return <div className="w-12 h-12 rounded-full bg-gray-200 animate-pulse" />;
+    return <div className="w-10 h-10 rounded-full bg-gray-200 animate-pulse" />;
 
   const { email } = userQuery.data;
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="w-12 h-12 rounded-full">
-        <img
-          className="w-12 h-12 rounded-full"
-          src="https://images.unsplash.com/photo-1608889175250-c3b0c1667d3a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
+      <DropdownMenuTrigger className="w-10 h-10 rounded-full">
+        <Avatar>
+          <AvatarImage
+            src="
+          https://images.unsplash.com/photo-1608889175250-c3b0c1667d3a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <AvatarFallback>User</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>{email}</DropdownMenuLabel>
