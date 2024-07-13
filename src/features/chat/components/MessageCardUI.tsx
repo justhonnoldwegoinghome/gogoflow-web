51ef1cc7c167c10ada0c0f2155ccdba2d7697453
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-07-13 13:57:15 +0800

    add reference to message

--- src/features/chat/components/MessageCardUI.tsx
+++ src/features/chat/components/MessageCardUI.tsx
@@ -1,5 +1,7 @@
 import clsx from "clsx";
 
+import { format } from "@/utils/format";
+
 import { Message } from "../types";
 
 interface MessageCardUIProps {
@@ -7,18 +9,29 @@ interface MessageCardUIProps {
 }
 
 export function MessageCardUI({ message }: MessageCardUIProps) {
-  const { sent_at, sender_role, text } = message;
+  const { sent_at, sender_role, text, is_supported } = message;
 
   return (
     <div
-      className={clsx("flex flex-col gap-2 p-4 rounded-lg border", {
+      className={clsx("p-4 rounded-lg border", {
         "border-blue-500 text-blue-500": sender_role === "seller",
       })}
     >
-      <p>{text ? text : "UNSUPPORTED"}</p>
-      <div className="text-gray-600 text-sm">
-        <p>{`Sent at: ${sent_at}`}</p>
-        <p>{`Sent by: ${sender_role}`}</p>
+      <p>{is_supported ? text : "UNSUPPORTED"}</p>
+
+      {message.reference && (
+        <div className="mt-4 text-gray-600 text-sm">
+          <p>
+            {message.reference.type === "product"
+              ? `Product ID:${message.reference.id}`
+              : `Order ID: ${message.reference.id}`}
+          </p>
+        </div>
+      )}
+
+      <div className="mt-4 text-end text-gray-600 text-sm">
+        <p>{format.date(new Date(sent_at))}</p>
+        <p>{format.time(new Date(sent_at))}</p>
       </div>
     </div>
   );
