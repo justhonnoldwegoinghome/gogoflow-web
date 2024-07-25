8061367c20c983ebd172482b1784379dafbf1499
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-07-25 17:37:08 +0800

    add sheet

---
+++ src/components/sheet/SheetTitle.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+
+import { cn } from "@/utils/cn";
+
+export const SheetTitle = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(function SheetTitle({ className, ...props }, ref) {
+  return (
+    <SheetPrimitive.Title
+      ref={ref}
+      className={cn("text-lg font-semibold text-foreground", className)}
+      {...props}
+    />
+  );
+});
---
+++ src/components/sheet/index.ts
@@ -0,0 +1,8 @@
+export { Sheet } from "./Sheet";
+export { SheetTrigger } from "./SheetTrigger";
+export { SheetContent } from "./SheetContent";
+export { SheetTitle } from "./SheetTitle";
+export { SheetHeader } from "./SheetHeader";
+export { SheetDescription } from "./SheetDescription";
+export { SheetFooter } from "./SheetFooter";
+export { SheetClose } from "./SheetClose";
