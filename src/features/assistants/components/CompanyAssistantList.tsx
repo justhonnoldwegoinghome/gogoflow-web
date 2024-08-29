0e874e789709657ca2d96813bc2b70f2e99b32a9
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-08-29 19:10:12 +0800

    🧹

--- src/features/assistants/components/CompanyAssistantList.tsx
+++ src/features/assistants/components/CompanyAssistantList.tsx
@@ -1,82 +1,3 @@
-// import clsx from "clsx";
-// import Link from "next/link";
-// import { Bot, Plus } from "lucide-react";
-
-// import { Button } from "@/components/button";
-// import { formatDate } from "@/utils";
-// import { Spinner } from "@/components/spinner";
-// import { Company } from "@/features/companies";
-
-// import { useCompanyAssistantList } from "../api/getCompanyAssistantList";
-// import { Assistant } from "../types";
-
-// interface CompanyAssistantListProps {
-//   id: Company["id"];
-//   selectedAssistantId: Assistant["id"] | null;
-// }
-
-// export function CompanyAssistantList({
-//   id,
-//   selectedAssistantId,
-// }: CompanyAssistantListProps) {
-//   const companyAssistantListQuery = useCompanyAssistantList({ id });
-
-//   if (!companyAssistantListQuery.data) return <Spinner />;
-
-//   if (companyAssistantListQuery.data.results.length === 0)
-//     return (
-//       <div className="flex flex-col gap-4 items-center h-full justify-center">
-//         <div className="w-16 h-16 bg-secondary rounded-full flex items-center justify-center">
-//           <Bot size={24} />
-//         </div>
-//         <div>
-//           <p className="text-center font-semibold">No bots found</p>
-//           <p className="text-center text-muted-foreground">
-//             Create a bot to get started.
-//           </p>
-//         </div>
-//         <div>
-//           <Button asChild>
-//             <Link
-//               href={`/c/${id}/create-bot`}
-//               className="flex gap-2 items-center"
-//             >
-//               <Plus size={16} />
-//               <p>Create</p>
-//             </Link>
-//           </Button>
-//         </div>
-//       </div>
-//     );
-
-//   return (
-//     <div className="w-full flex flex-col">
-//       {companyAssistantListQuery.data.results.map((a) => (
-//         <Link key={a.id} href={`/c/${id}/bots/${a.id}`}>
-//           <div
-//             className={clsx(
-//               "p-3 rounded-lg flex gap-4 justify-between items-center hover:bg-secondary",
-//               {
-//                 "bg-secondary": a.id === selectedAssistantId,
-//               }
-//             )}
-//           >
-//             <div>
-//               <p className="text-sm font-semibold">{a.name}</p>
-//               <p className="text-xs text-muted-foreground">{a.id}</p>
-//             </div>
-//             <div>
-//               <p className="text-xs text-muted-foreground">
-//                 {formatDate(new Date(a.created_at))}
-//               </p>
-//             </div>
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
-
 import { Plus, ChevronsUpDown, Bot } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -90,10 +11,10 @@ import {
   DropdownMenuGroup,
 } from "@/components/dropdownMenu";
 import { Button } from "@/components/button";
+import { Company } from "@/features/companies";
 
 import { Assistant } from "../types";
 import { useCompanyAssistantList } from "../api/getCompanyAssistantList";
-import { Company } from "@/features/companies";
 
 interface CompanyAssistantListProps {
   id: Company["id"];
