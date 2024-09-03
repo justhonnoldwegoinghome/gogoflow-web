9983dfb2f425fe047f6070aaecc159bc9c7b6e01
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-09-03 10:49:28 +0800

    style

--- src/features/messages/components/SendMessage.tsx
+++ src/features/messages/components/SendMessage.tsx
@@ -1,12 +1,11 @@
 import _ from "lodash";
 import { useState } from "react";
 
-import { Textarea } from "@/components/form";
 import { Button } from "@/components/button";
 import { Company } from "@/features/companies";
+import { Conversation } from "@/features/conversations";
 
 import { useSendMessage } from "../api/sendMessage";
-import { Conversation } from "../../conversations/types";
 
 interface SendMessageProps {
   companyId: Company["id"];
@@ -22,33 +21,30 @@ export function SendMessage({ companyId, conversationId }: SendMessageProps) {
   const [text, setText] = useState("");
 
   return (
-    <div className="bg-white">
+    <div className="bg-white flex flex-col gap-2">
       <textarea
-        className="ring-2 ring-input focus-visible:ring-ring w-full rounded-lg px-4 py-2 laptop:shadow-lg focus:outline-none"
+        className="ring-2 ring-input focus-visible:ring-ring w-full rounded-md px-4 py-2 laptop:shadow-lg focus:outline-none"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <br />
-      <div className="ml-auto w-fit">
-        <Button
-          disabled={!text}
-          isLoading={sendMessageMutation.isMutating}
-          onClick={() =>
-            sendMessageMutation
-              .trigger({
-                conversationId,
-                data: {
-                  source: "shopee",
-                  companyId,
-                  text,
-                },
-              })
-              .then(() => setText(""))
-          }
-        >
-          Send
-        </Button>
-      </div>
+      <Button
+        disabled={!text}
+        isLoading={sendMessageMutation.isMutating}
+        onClick={() =>
+          sendMessageMutation
+            .trigger({
+              conversationId,
+              data: {
+                source: "shopee",
+                companyId,
+                text,
+              },
+            })
+            .then(() => setText(""))
+        }
+      >
+        Send
+      </Button>
     </div>
   );
 }
