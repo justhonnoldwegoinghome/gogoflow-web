9fb5489965bbd4965c4e09d6076915e5c50d151c
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-09-24 14:20:50 +0800

    remove tmp page

--- src/pages/tmp.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="h-[200vh] bg-blue-200 relative">
-      <div className="h-12 bg-teal-100 fixed bottom-0 w-full"></div>
-    </div>
-  );
-}
