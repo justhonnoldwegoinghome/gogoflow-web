2209cd5ad0ebc292e9db4e30dfb9eb9667a95dff
justhonnoldwegoinghome <wesleytan19@gmail.com>
2024-10-03 09:04:09 +0800

    clear swr cache upon logging out

--- src/features/authentication/api/logOut.ts
+++ src/features/authentication/api/logOut.ts
@@ -1,27 +1,13 @@
 import useSWRMutation from "swr/mutation";
-import { useRouter } from "next/router";
 
 import { post } from "@/apiClient";
-
-import { useAuthStore } from "../stores/useAuthStore";
 
 export function logOut() {
   return post<string>("/auth/log-out", {});
 }
 
 export function useLogOut() {
-  const _logOut = useAuthStore((s) => s.logOut);
-
-  const push = useRouter().push;
-
-  return useSWRMutation(
-    "/log-out",
-    () =>
-      logOut()
-        .then(_logOut)
-        .then(() => push("/")),
-    {
-      throwOnError: false,
-    }
-  );
+  return useSWRMutation("/log-out", () => logOut(), {
+    throwOnError: false,
+  });
 }
--- src/features/users/components/UserDropdownMenu.tsx
+++ src/features/users/components/UserDropdownMenu.tsx
@@ -1,4 +1,6 @@
 import { Settings, LogOut, User2 } from "lucide-react";
+import { useCallback } from "react";
+import { useSWRConfig } from "swr";
 import { useRouter } from "next/router";
 
 import {
@@ -8,7 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuGroup,
 } from "@/components/dropdownMenu";
-import { useLogOut } from "@/features/authentication";
+import { useAuthStore, useLogOut } from "@/features/authentication";
 
 import { useUser } from "../api/getUser";
 import { User } from "../types";
@@ -19,6 +21,14 @@ interface UserDropdownMenuProps {
 
 export function UserDropdownMenu({ id }: UserDropdownMenuProps) {
   const { push } = useRouter();
+  const { mutate } = useSWRConfig();
+  const logOutFromAuthStore = useAuthStore((s) => s.logOut);
+
+  const onLogOut = useCallback(() => {
+    logOutFromAuthStore();
+    mutate(() => true, undefined, { revalidate: false });
+    push("/");
+  }, []);
 
   const logOutMutation = useLogOut();
 
@@ -30,25 +40,29 @@ export function UserDropdownMenu({ id }: UserDropdownMenuProps) {
     );
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className="w-8 h-8 flex items-center justify-center bg-white rounded-full focus-visible:ring-2 ring-ring focus-visible:outline-none border">
-          <User2 size={16} className="text-muted-foreground" />
-        </button>
-      </DropdownMenuTrigger>
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="w-8 h-8 flex items-center justify-center bg-white rounded-full focus-visible:ring-2 ring-ring focus-visible:outline-none border">
+            <User2 size={16} className="text-muted-foreground" />
+          </button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent>
-        <DropdownMenuGroup>
-          <DropdownMenuItem onSelect={() => push("/me")}>
-            <Settings className="mr-3 h-4 w-4" />
-            <span>Account</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem onSelect={() => logOutMutation.trigger()}>
-            <LogOut className="mr-3 h-4 w-4" />
-            Log out
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuItem onSelect={() => push("/me")}>
+              <Settings className="mr-3 h-4 w-4" />
+              <span>Account</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={() => logOutMutation.trigger().then(onLogOut)}
+            >
+              <LogOut className="mr-3 h-4 w-4" />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
